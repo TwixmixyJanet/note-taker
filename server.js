84fe862bdbb3ea6c28de1732f5d777a56ec08567
Janet Webster <twixmixy@gmail.com>
2023-10-26 09:56:11 -0400

    removed routes, trying to get file pathing to work

--- server.js
+++ server.js
@@ -26,43 +26,43 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-generateNewNote = (body, notesArray) => {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
+// createNewNote = (body, notesArray) => {
+//     const newNote = body;
+//     if (!Array.isArray(notesArray))
+//         notesArray = [];
 
-    if (notesArray.length === 0)
-        notesArray.push(0);
+//     if (notesArray.length === 0)
+//         notesArray.push(0);
 
-    body.id = notesArray[0];
-    notesArray[0]++;
+//     body.id = notesArray[0];
+//     notesArray[0]++;
 
-    notesArray.push(newNote);
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2));
+//     notesArray.push(newNote);
+//     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2));
 
-    return newNote;
-}
+//     return newNote;
+// }
 
-removeNote = (id, notesArray) => {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+// deleteNote = (id, notesArray) => {
+//     for (let i = 0; i < notesArray.length; i++) {
+//         let note = notesArray[i];
         
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2));
+//         if (note.id == id) {
+//             notesArray.splice(i, 1);
+//             fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2));
 
-            break;
-        }
-    }
-}
+//             break;
+//         }
+//     }
+// }
 
 app.post('/api/notes', (req, res) => {
-    const newNote = generateNewNote(req.body, allNotes);
+    const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    removeNote(req.params.id, allNotes);
+    deleteNote(req.params.id, allNotes);
     res.json(true);
 });
 
