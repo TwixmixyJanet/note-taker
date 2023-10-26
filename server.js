9d64a5db831a7054baf72380b074d67c8967d09a
Janet Webster <twixmixy@gmail.com>
2023-10-26 08:57:28 -0400

    GET requests

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(databaseNotes.slice(1));
+});
+
